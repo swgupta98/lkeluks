47ecd659ffa1d7a70145cee137dc69a29066e003
Swarnim <swgupta@akamai.com>
2023-02-08 17:13:52 +0530

    updated tang server url

--- luks_setup.sh
+++ luks_setup.sh
@@ -112,11 +112,9 @@ EOF
 cat > /etc/crypttab <<EOF
 secure	UUID=`lsblk --nodeps --noheadings -o UUID /dev/sdb3`	none
 EOF
-echo "debug line 1"
 update-grub
-echo "debug line 2"
 grub-install /dev/sdb
-echo "debug line 3"
+echo $LUKS_KEY | clevis luks bind -y -d /dev/sdb3 tang '{"url": "http://170.187.131.224"}' -k -
 mkdir /var/tmp
 update-initramfs -u -k 'all'
 EOFF
